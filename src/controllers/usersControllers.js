0a85549c3e02bdca3ca35c4ba2336909a1deb1b2
sebastien_lacour <sebastien-lacour@hotmail.com>
2023-11-29 11:12:56 +0100

    done

---
+++ src/controllers/usersControllers.js
@@ -0,0 +1,35 @@
+const database = require("../../database");
+
+const getUsers = (req, res) => {
+  database
+    .query("select * from users")
+    .then(([users]) => {
+      res.status(200).json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const getUsersById = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+    .query("select * from users where id = ?", [id])
+    .then(([users]) => {
+      if (users[0] != null) {
+        res.status(200).json(users[0]);
+      } else {
+        res.sendStatus(404);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+module.exports = {
+  getUsers,
+  getUsersById,
+};
